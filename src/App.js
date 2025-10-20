b9c54bbb6bee483c16221ffd15b844aff93c9342
Henil5204 <101511850@georgebrown.ca>
2025-10-20 10:52:32 -0400

    add lab 7

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <h2 className='intro'>Welcome to Full-Stack Development - 1 </h2>
+       <h3 className='lang'>React JS Programming Week09 Lab exercise</h3>
+       <h5 className='ID'>101511850 </h5>
+       <h6 className='Std_n'>Henil Patel</h6>
+       <h6 className='clg'>George Brown College, Toronto </h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
